8b66ade949909d7a5ceafd95556bc8bede1ad85f
FenosoaTiavina <fenosoa.t.r@gmail.com>
2025-03-15 17:32:40 +0300

    fix

--- src/app/pages/pages.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ClientComponent } from './client/home/home.component.ts';
-
-export const PagesRoutes: Routes = [
-  {
-    path: 'home',
-    component: ClientComponent,
-  },
-];
