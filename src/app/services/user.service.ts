d145669deda76ba4232c3509102de5abb28ab8d3
FenosoaTiavina <fenosoa.t.r@gmail.com>
2025-03-16 13:12:21 +0300

    Fix env for prod

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -21,7 +21,6 @@ export class UserService {
 
   getCurrentUser():Observable<any> {
     const user_id = this.cookieService.get('userId');
-    console.log(user_id);
     return this.http.get(`${this.apiUrl}/${user_id}`).pipe(
       catchError((error: HttpErrorResponse) => {
         return of({error : error.error.error}); // This is your fallback value
