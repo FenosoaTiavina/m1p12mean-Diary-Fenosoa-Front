52c88fc5e06163e0f0e5f35c5677180cfff521de
FenosoaTiavina <fenosoa.t.r@gmail.com>
2025-03-16 20:04:43 +0300

    Save

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
-  apiUrl: "https://m1p12mean-diary-fenosoa-back-production.up.railway.app" // URL pour le développement
-=======
   apiUrl: 'https://m1p12mean-diary-fenosoa-back-production.up.railway.app' // URL pour le développement
->>>>>>> dev
 };
