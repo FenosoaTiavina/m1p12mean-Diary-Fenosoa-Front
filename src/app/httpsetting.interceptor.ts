ba9199e48fb76207ad186db371998e4698c42841
FenosoaTiavina <fenosoa.t.r@gmail.com>
2025-03-16 15:31:18 +0300

    Auth header

--- src/app/httpsetting.interceptor.ts
+++ src/app/httpsetting.interceptor.ts
@@ -6,6 +6,9 @@ export const HttpSettingInterceptor: HttpInterceptorFn = (req, next) => {
   const cookie = inject(CookieService);
   const authReq = req.clone({
     withCredentials: true,
+    headers: new HttpHeaders({
+      'Authorization': `bearer ${cookie.get("accessToken")} `
+    })
   });
   return next(authReq);
 };
