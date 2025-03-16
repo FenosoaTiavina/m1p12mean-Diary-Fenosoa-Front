c69615a8be295fe31254ca0f147b73767db9ee86
FenosoaTiavina <fenosoa.t.r@gmail.com>
2025-03-16 15:19:06 +0300

    ! work

--- src/app/httpsetting.interceptor.ts
+++ src/app/httpsetting.interceptor.ts
@@ -6,9 +6,6 @@ export const HttpSettingInterceptor: HttpInterceptorFn = (req, next) => {
   const cookie = inject(CookieService);
   const authReq = req.clone({
     withCredentials: true,
-    headers: new HttpHeaders({
-      'cookie': `accessToken=${cookie.get("accessToken")}; `+ `refreshToken=${cookie.get("refreshToken")}; `
-    })
   });
   return next(authReq);
 };
