900e092715997eb40edee8dbc6f58948d95d57ad
FenosoaTiavina <fenosoa.t.r@gmail.com>
2025-03-16 15:03:30 +0300

    Typo & cookie fic for xhrome browser

--- src/app/httpsetting.interceptor.ts
+++ src/app/httpsetting.interceptor.ts
@@ -1,8 +1,14 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn , HttpHeaders} from '@angular/common/http';
+import { inject } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 export const HttpSettingInterceptor: HttpInterceptorFn = (req, next) => {
+  const cookie = inject(CookieService);
   const authReq = req.clone({
-    withCredentials: true
+    withCredentials: true,
+    headers: new HttpHeaders({
+      'cookie': `accessToken=${cookie.get("accessToken")}; `+ `refreshToken=${cookie.get("refreshToken")}; `
+    })
   });
   return next(authReq);
 };
